511a2b932850f703cbdbd4dc71dd3be4e273ff8c
Super-Matter <martidont2@mail.ru>
2021-03-01 14:41:42 +0300

    Working on firmware

--- Big_Wheel_Macro_Pad_Firmware/actions.h
+++ Big_Wheel_Macro_Pad_Firmware/actions.h
@@ -10,74 +10,74 @@
 // Note that the special shuttling behavior isn't in this file, since it needs to be processed separately
 
 // This maps matrix key positions onto their logical control ID's
-const byte keyMatrix[2][3][3] = {
+const byte keyMatrix[9][2] = {
   {
-    {2, 1, 0},
-    {8, 7, 11},
-    {17, 12, 15}
-  },
-  {
-    {5, 3, 4},
-    {9, 16, 13},
-    {6, 14, 10}
+    {2, 1},   // E1 + 4
+    {8, 7},   // 6 + 5
+    {17, 12}, // E2 + 3
+    {17, 12}, // E3 + 2
+    {17, 12}, // E4 + 1
+    {17, 12}, // 8 + 7
+    {17, 12}, // 10 + 9
+    {17, 12}, // 12 + 11
+    {17, 12}  // 14 + 13
   }
-};
 
- const int switchAssignments[] = 
- {KEY_F, KEY_Q, KEY_W, KEY_I, KEY_O, 
- KEY_M, ACTION_AUDIO_CLIP_MIXER_WINDOW, ACTION_SOURCE_MONITOR_WINDOW, KEY_V, KEY_QUOTE,
- KEY_BACKSLASH, ACTION_TIMELINES_WINDOW, KEY_A, KEY_COMMA, ACTION_JUMP_TO_IN_POINT,
-  ACTION_CTRL_KEY, ACTION_UNDO, 
-  NO_ACTION  // I wired the big knob click into the circuit, even though pressing it is physically impossible
+
+  const int switchAssignments[] =
+  { KEY_F, KEY_Q, KEY_W, KEY_I, KEY_O,
+    KEY_M, ACTION_AUDIO_CLIP_MIXER_WINDOW, ACTION_SOURCE_MONITOR_WINDOW, KEY_V, KEY_QUOTE,
+    KEY_BACKSLASH, ACTION_TIMELINES_WINDOW, KEY_A, KEY_COMMA, ACTION_JUMP_TO_IN_POINT,
+    ACTION_CTRL_KEY, ACTION_UNDO,
+    NO_ACTION  // I wired the big knob click into the circuit, even though pressing it is physically impossible
   };
 
-// CCW, CW
-const int topKnobAssignments[] = {KEY_RIGHT_BRACE, KEY_LEFT_BRACE};
-const int middleKnobAssignments[] = {KEY_EQUAL, KEY_MINUS};
-const int lowerKnobAssignments[] = {KEY_DOWN, KEY_UP};
-const int wheelAssignments[] = {KEY_RIGHT, KEY_LEFT}; 
+  // CCW, CW
+  const int topKnobAssignments[] = {KEY_RIGHT_BRACE, KEY_LEFT_BRACE};
+  const int middleKnobAssignments[] = {KEY_EQUAL, KEY_MINUS};
+  const int lowerKnobAssignments[] = {KEY_DOWN, KEY_UP};
+  const int wheelAssignments[] = {KEY_RIGHT, KEY_LEFT};
 
-void performAction(int action) {
-  switch (action) {
-    case NO_ACTION:
-      break;
-    case ACTION_SOURCE_MONITOR_WINDOW:
-      Keyboard.press(MODIFIERKEY_SHIFT);
-      Keyboard.press(KEY_2);
-      Keyboard.release(KEY_2);
-      Keyboard.release(MODIFIERKEY_SHIFT);
-      break;
-    case ACTION_TIMELINES_WINDOW:
-      Keyboard.press(MODIFIERKEY_SHIFT);
-      Keyboard.press(KEY_3);
-      Keyboard.release(KEY_3);
-      Keyboard.release(MODIFIERKEY_SHIFT);
-      break;
-    case ACTION_AUDIO_CLIP_MIXER_WINDOW:
-      Keyboard.press(MODIFIERKEY_SHIFT);
-      Keyboard.press(KEY_9);
-      Keyboard.release(KEY_9);
-      Keyboard.release(MODIFIERKEY_SHIFT);
-      break;
+  void performAction(int action) {
+    switch (action) {
+      case NO_ACTION:
+        break;
+      case ACTION_SOURCE_MONITOR_WINDOW:
+        Keyboard.press(MODIFIERKEY_SHIFT);
+        Keyboard.press(KEY_2);
+        Keyboard.release(KEY_2);
+        Keyboard.release(MODIFIERKEY_SHIFT);
+        break;
+      case ACTION_TIMELINES_WINDOW:
+        Keyboard.press(MODIFIERKEY_SHIFT);
+        Keyboard.press(KEY_3);
+        Keyboard.release(KEY_3);
+        Keyboard.release(MODIFIERKEY_SHIFT);
+        break;
+      case ACTION_AUDIO_CLIP_MIXER_WINDOW:
+        Keyboard.press(MODIFIERKEY_SHIFT);
+        Keyboard.press(KEY_9);
+        Keyboard.release(KEY_9);
+        Keyboard.release(MODIFIERKEY_SHIFT);
+        break;
       case ACTION_UNDO:
-      Keyboard.press(MODIFIERKEY_GUI);  // I think I remapped this on my PC. Use MODIFIERKEY_CTRL if this doesn't work
-      Keyboard.press(KEY_Z);
-      Keyboard.release(KEY_Z);
-      Keyboard.release(MODIFIERKEY_GUI);
-      break;
+        Keyboard.press(MODIFIERKEY_GUI);  // I think I remapped this on my PC. Use MODIFIERKEY_CTRL if this doesn't work
+        Keyboard.press(KEY_Z);
+        Keyboard.release(KEY_Z);
+        Keyboard.release(MODIFIERKEY_GUI);
+        break;
       case ACTION_JUMP_TO_IN_POINT:
-      Keyboard.press(MODIFIERKEY_SHIFT);
-      Keyboard.press(KEY_I);
-      Keyboard.release(KEY_I);
-      Keyboard.release(MODIFIERKEY_SHIFT);
-      break;
-    case ACTION_CTRL_KEY:
-      Keyboard.press(MODIFIERKEY_GUI);
-      Serial.println("Ctrl down");
-      break;
-    default:
-      Keyboard.press(action);
-      Keyboard.release(action);
+        Keyboard.press(MODIFIERKEY_SHIFT);
+        Keyboard.press(KEY_I);
+        Keyboard.release(KEY_I);
+        Keyboard.release(MODIFIERKEY_SHIFT);
+        break;
+      case ACTION_CTRL_KEY:
+        Keyboard.press(MODIFIERKEY_GUI);
+        Serial.println("Ctrl down");
+        break;
+      default:
+        Keyboard.press(action);
+        Keyboard.release(action);
+    }
   }
-}
-
--- Big_Wheel_Macro_Pad_Firmware/pins.h
+++ Big_Wheel_Macro_Pad_Firmware/pins.h
@@ -1,44 +1,36 @@
-#define topKnobA 0
-#define topKnobB 1
-#define middleKnobA 17
-#define middleKnobB 16
-#define lowerKnobA 15
-#define lowerKnobB 14
-#define wheelA 11
-#define wheelB 10
+// Defining encoder pins responsible for "rotating"
+#define E1_S1 1
+#define E1_S2 0
+#define E2_S1 8
+#define E2_S2 9
+#define E3_S1 16
+#define E3_S2 10
+#define E4_S1 15
+#define E4_S2 14
 
-const byte allKnobPins[] = {topKnobA, topKnobB,
-                      middleKnobA, middleKnobB,
-                      lowerKnobA, lowerKnobB,
-                      wheelA, wheelB
-                     };
-
-// To simplify wiring, keys are actually in two separate matrices.
-// The Teensy has like a million GPIO's, I can sacrifice four.
-#define leftButtonsA 23
-#define leftButtonsB 22
-#define leftButtonsC 18
-#define leftButtons1 21
-#define leftButtons2 20
-#define leftButtons3 19
-
-#define rightButtonsA 2
-#define rightButtonsB 6
-#define rightButtonsC 7
-#define rightButtons1 3
-#define rightButtons2 5
-#define rightButtons3 4
-
-const byte leftButtonRowPins[] = {leftButtonsA, leftButtonsB, leftButtonsC};
-const byte rightButtonRowPins[] = {rightButtonsA, rightButtonsB, rightButtonsC};
-
-const byte allButtonRowPins[] = {leftButtonsA, leftButtonsB, leftButtonsC,
-                           rightButtonsA, rightButtonsB, rightButtonsC
+const byte allEncPins[] = { E1_S1, E1_S2, // TopRightEnc
+                            E2_S1, E2_S2, // MiddleRightEnc
+                            E3_S1, E3_S2, // BottomRightEnc
+                            E4_S1, E4_S2  // WheelEnc
                           };
 
-const byte leftButtonColumnPins[] = {leftButtons1, leftButtons2, leftButtons3};
-const byte rightButtonColumnPins[] = {rightButtons1, rightButtons2, rightButtons3};
+// To simplify wiring I got two columns and nine rows = 11 pins in total.
+// But one column is gnd, so that's 10 pins for 18 buttons.
+// Pro Micro is lacking pins comparing to Teensy.
+// Look at wiring diagram for better understandig
+#define button1_E4 7
+#define button2_E3 6
+#define button3_E2 5
+#define button4_E1 2
+#define button5_6 3
+#define button7_8 21
+#define button9_10 20
+#define button11_12 19
+#define button13_14 18
+#define orangeLine A6
 
-const byte allButtonColumnPins[] = {leftButtons1, leftButtons2, leftButtons3,
-                              rightButtons1, rightButtons2, rightButtons3
-                             };
+
+const byte allButtonRowPins[] = {button1_E4, button2_E3, button3_E2,
+                                 button4_E1, button5_6, button7_8,
+                                 button9_10, button11_12, button13_14
+                                };
